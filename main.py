a3f31b9e788295ca4897b375d4494a2666161b22
HaesungAhn <biamqz@gmail.com>
2024-01-08 15:14:06 +0900

    합산 주석처리버전

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ class WindowClass(QMainWindow, form_class) :
         self.setupUi(self)
 
         self.menu = {'김밥': 2000, '라면': 4000, '떡볶이': 4000, '순대': 3000 }
+        self.cart_items = [] #각 메뉴의 가격과 개수를 저장하는 리스트
+
         for i in range(1, 5):
             button_object_name = f"Button_menu{i}"
             menu_button = getattr(self, button_object_name, None)
@@ -29,7 +31,34 @@ class WindowClass(QMainWindow, form_class) :
             item_text = f"{menu_text} - {price}원"
             item = QListWidgetItem(item_text)
             cart_list_widget.addItem(item)
+
         return handler
+        #     for item in self.cart_items:
+        #         if item['name'] == menu_text: #메뉴가 장바구니에 있는지 확인, 있다면 개수증가
+        #             item['count'] += 1
+        #             item['total price'] = item['count'] * price
+        #             break
+        #         else:
+        #             new_item = {'name': menu_text, 'count': 1, 'total_price': price}
+        #             self.cart_items.append(new_item)
+        #
+        #     self.update_cart_widget(cart_list_widget)
+        #
+        # return handler
+
+
+
+    # def update_cart_widget(self, cart_list_widget):
+    #     cart_list_widget.clear()
+    #
+    #     total_price = 0
+    #     for item in self.cart_items:
+    #         item.text = f"{item['name']} - {item['total_price']}원 ({item['count']}개)"
+    #         total_price += item['total_price']
+    #         cart_list_widget.addItem(item.text)
+    #
+    #     total_text = f"총 가격: {total_price}원"
+    #     cart_list_widget.addItem(total_text)
 
 if __name__ == "__main__" :
     #QApplication : 프로그램을 실행시켜주는 클래스
